773791d486c32e7528881bd4a0c3d1eb4437966a
Farelart <ffjdganlaky@gmail.com>
2025-02-13 17:43:30 +0100

    fixing major dependencies issues

--- server/react-dom-server-shim.js
+++
@@ -1,7 +0,0 @@
-import ReactDOMServer from 'react-dom/server';
-
-module.exports = {
-  ...ReactDOMServer,
-  // Provide a fallback; note: renderToReadableStream isn’t a full substitute for streaming
-  renderToReadableStream: ReactDOMServer.renderToString,
-};
